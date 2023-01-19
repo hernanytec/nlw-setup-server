156b3411baca5edd1efd86ebde26fd3daf28bd60
hernany <hernanytec777@gmail.com>
2023-01-19 17:04:16 -0300

    isolate routes and prisma instance

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,12 @@
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
 import Fastify from 'fastify'
+import { appRoutes } from './routes'
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
+app.register(appRoutes)
 
-app.get('/habits', async () => {
-  const habits =  await prisma.habit.findMany()
-  return habits  
-})
-
-app.listen({port: 3333}).then(() => {
+app.listen({ port: 3333 }).then(() => {
   console.log('server is running')
 })
